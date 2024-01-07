106a6b47ecc5e3b46592a7bbad30544526df9b21
nick747 <micco.mario@gmail.com>
2024-01-07 18:00:42 +0100

    added languages' badges

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,21 +1,34 @@
+<script lang="ts">
+  import Language from "./language.svelte";
+</script>
+
 <div class="about">
   <h2>About</h2>
   <p>
     Hey there! I go by Nick747, but feel free to call me Mario. I&apos;m a teen
     based in Italy who enjoys developing with website and mobile app development
-    for fun. I&apos;ve experience in vanilla HTML, CSS, and JavaScript, and
-    these days, I&apos;m exploring tools like React, Svelte, TailwindCSS, and
-    Astro. My passion lies in crafting mobile apps using Flutter & Dart. While
-    I&apos;m still getting the hang of it, I also have some basic familiarity
-    with languages like C/C++, Python and Kotlin. When it comes to coding,
-    I&apos;m all about VS Code, though I switch over to Neovim on my Linux
-    machine. Outside of school, I spend my free time playing around with designs
-    for websites, apps, or just about any service that sparks my interest.
-    Looking ahead, I&apos;m planning to attend university and pursue a career in
-    the industry, maybe even start my own company someday! Also, in case
-    you&apos;re wondering, I&apos;m fluent in Italian (it&apos;s my native
-    language), I&apos;m fairly proficient in English (B2), and
-    I&apos;m currently diving into learning Russian.
+    for fun. I&apos;ve experience in vanilla <Language language="HTML" />, <Language
+      language="CSS"
+    />, and <Language language="JavaScript" />, and these days, I&apos;m
+    exploring tools like <Language language="React" />, <Language
+      language="Svelte"
+    />, <Language language="TailwindCSS" />, and <Language language="Astro" />.
+    My passion lies in crafting mobile apps using <Language
+      language="Flutter"
+    /> & <Language language="Dart" />. While I&apos;m still getting the hang of
+    it, I also have some basic familiarity with languages like <Language
+      language="C"
+    />/<Language language="CPP" />, <Language language="Python" /> and <Language
+      language="Kotlin"
+    />. When it comes to coding, I&apos;m all about <Language
+      language="VSCode"
+    />, though I switch over to <Language language="NeoVim" /> on my Linux machine.
+    Outside of school, I spend my free time playing around with designs for websites,
+    apps, or just about any service that sparks my interest. Looking ahead, I&apos;m
+    planning to attend university and pursue a career in the industry, maybe even
+    start my own company someday! Also, in case you&apos;re wondering, I&apos;m fluent
+    in Italian (it&apos;s my native language), I&apos;m fairly proficient in English
+    (B2), and I&apos;m currently diving into learning Russian.
   </p>
 </div>
 
@@ -28,5 +41,7 @@
 
   p {
     font-size: 18px;
+    line-height: 2;
+    margin-bottom: 3rem;
   }
 </style>
---
+++ src/components/language.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  let languages = new Map<string, string>([
+    ["HTML", "src/assets/imgs/HTML.svg"],
+    ["CSS", "src/assets/imgs/CSS.svg"],
+    ["JavaScript", "src/assets/imgs/JavaScript.svg"],
+    ["React", "src/assets/imgs/React-Dark.svg"],
+    ["Svelte", "src/assets/imgs/Svelte.svg"],
+    ["TailwindCSS", "src/assets/imgs/TailwindCSS-Dark.svg"],
+    ["Astro", "src/assets/imgs/Astro.svg"],
+    ["Flutter", "src/assets/imgs/Flutter-Dark.svg"],
+    ["Dart", "src/assets/imgs/Dart-Dark.svg"],
+    ["C", "src/assets/imgs/C.svg"],
+    ["CPP", "src/assets/imgs/CPP.svg"],
+    ["VSCode", "src/assets/imgs/VSCode-Dark.svg"],
+    ["NeoVim", "src/assets/imgs/NeoVim-Dark.svg"],
+    ["Python", "src/assets/imgs/Python-Dark.svg"],
+    ["Kotlin", "src/assets/imgs/Kotlin-Dark.svg"],
+  ]);
+
+  export let language: string;
+  let imgUrl = languages.get(language);
+</script>
+
+<div class="language">
+  <img src={imgUrl} alt={"Product icon of " + language} />
+  <p>{language}</p>
+</div>
+
+<style>
+  .language {
+    display: inline-flex;
+    align-items: center;
+    gap: 0.25rem;
+    border-radius: .5rem;
+    border: 1px #363636 solid;
+    background-color: #242424;
+    padding: .25rem;
+    font-size: 0.875rem;
+    line-height: 1.25;
+    color: #1a202c;
+    text-decoration: none;
+  }
+
+  img {
+    height: 18px;
+    aspect-ratio: 1;
+  }
+
+  p {
+    font-size: 18px;
+    font-weight: 500;
+  }
+</style>
