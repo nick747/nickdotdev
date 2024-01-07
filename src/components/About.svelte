973bd6e58acc9261e85262420e6465be5e467e7c
nick747 <micco.mario@gmail.com>
2024-01-07 17:00:36 +0100

    created base for about section and uploaded langs icons

---
+++ src/components/About.svelte
@@ -0,0 +1,32 @@
+<div class="about">
+  <h2>About</h2>
+  <p>
+    Hey there! I go by Nick747, but feel free to call me Mario. I&apos;m a teen
+    based in Italy who enjoys developing with website and mobile app development
+    for fun. I&apos;ve experience in vanilla HTML, CSS, and JavaScript, and
+    these days, I&apos;m exploring tools like React, Svelte, TailwindCSS, and
+    Astro. My passion lies in crafting mobile apps using Flutter & Dart. While
+    I&apos;m still getting the hang of it, I also have some basic familiarity
+    with languages like C/C++, Python and Kotlin. When it comes to coding,
+    I&apos;m all about VS Code, though I switch over to Neovim on my Linux
+    machine. Outside of school, I spend my free time playing around with designs
+    for websites, apps, or just about any service that sparks my interest.
+    Looking ahead, I&apos;m planning to attend university and pursue a career in
+    the industry, maybe even start my own company someday! Also, in case
+    you&apos;re wondering, I&apos;m fluent in Italian (it&apos;s my native
+    language), I&apos;m fairly proficient in English (B2), and
+    I&apos;m currently diving into learning Russian.
+  </p>
+</div>
+
+<style>
+  h2 {
+    font-size: 40px;
+    font-weight: 600;
+    margin-bottom: 1rem;
+  }
+
+  p {
+    font-size: 18px;
+  }
+</style>
